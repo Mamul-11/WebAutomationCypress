c5abc1d7645d9f5e23028cc54cf703138463e4a5
Mochammad Itmamul Wafa <itmamulwafa11@yahoo.com>
2023-02-08 23:31:47 +0700

    tambahan customerLogin, login.data, customerLogin.page, customerlogin.test, login.test

---
+++ test/data/customerLogin.data.js
@@ -0,0 +1,16 @@
+import * as generator from '@helpers/generator'
+
+
+// export const VALID_LOGIN ={
+//     username : 'problem_user',
+//     password : 'secret_sauce'
+// }
+
+// export const INVALID_LOGIN ={
+//     username : generator.username(),
+//     password : generator.password()
+// }
+
+export const LIST_CUSTOMER ={
+    
+}
--- test/page/CustomerLogin.page.js
+++ test/page/CustomerLogin.page.js
@@ -1,2 +1,3 @@
 export const dropdownField = '#userSelect'
 export const loginButton = '.btn btn-default'             //Use class
+export const customerLogin = '[ng-click="customer()"]'             //Use class
