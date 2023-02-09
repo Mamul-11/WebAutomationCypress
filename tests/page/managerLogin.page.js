36db4b9ac8e91d1e22e03c3440d8d7397061fd45
Mochammad Itmamul Wafa <itmamulwafa11@yahoo.com>
2023-02-09 18:59:10 +0700

    hahaha

--- tests/page/managerLogin.page.js
+++ tests/page/managerLogin.page.js
@@ -1,7 +1,21 @@
-export const managerLogin = '[ng-click="manager()"]' 
+//button manager login
+export const managerLogin = 'body > div.ng-scope > div > div.ng-scope > div > div.borderM.box.padT20 > div:nth-child(3) > button' 
+
 export const addCustomer = '[ng-class="btnClass1"]'
 export const firstName = '[ng-model="fName"]'
 export const lastName = '[ng-model="lName"]'
 export const postCode = '[ng-model="postCd"]'
 export const addCustomerButton = '[class="btn btn-default"]'
 export const alertAddCus = '.command-info'
+
+//open acount
+export const openAcount = '[ng-class="btnClass2"]'
+export const customerSelect = '#userSelect'
+export const currencySelect = '#currency'
+export const submitOpen = 'body > div.ng-scope > div > div.ng-scope > div > div.ng-scope > div > div > form > button'
+
+//csutomers
+export const customers = '[ng-click="showCust()"]'
+export const search = '[ng-model="searchCustomer"]'
+export const del   = 'body > div.ng-scope > div > div.ng-scope > div > div.ng-scope > div > div > table > tbody > tr:nth-child(4) > td:nth-child(1)'
+export const deleteButton = '[ng-click="deleteCust(cust)"]'
